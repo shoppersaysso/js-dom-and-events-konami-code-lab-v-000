5b46a32b07e9bf9483cf1ab7b58514e130762078
Sara Hopper <sara.hopper@gmail.com>
2017-10-02 00:06:24 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,8 +3,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   const body = document.querySelector('body')
 
-  body.addEventListener('keydown', function(e) {
-    if (e.which === 71) {
+  body.addEventListener('keydown', function(input) {
+    if (input.which === code) {
       return e.preventDefault()
     }
   })
