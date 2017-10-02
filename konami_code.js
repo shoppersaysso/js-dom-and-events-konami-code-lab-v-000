a205bb4870c3859ef4e8cb848d3871a21aa512c9
Sara Hopper <sara.hopper@gmail.com>
2017-10-02 00:11:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,25 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init(){
+  function onKeyDownHandler(e) {
+    let index = 0;
 
-}
+     document.body.addEventListener('keydown', function(e) {
+       const key = parseInt(e.which || e.detail);
 
-function onKeyDownHandler(e) {
-  let index = 0;
+       if (code[index] === key) {
+         index++;
 
-   document.body.addEventListener('keydown', function(e) {
-     const key = parseInt(e.which || e.detail);
+         if (index === code.length) {
+           alert('Hurray!');
 
-     if (code[index] === key) {
-       index++;
-
-       if (index === code.length) {
-         alert('Hurray!');
-
+           index = 0;
+         }
+       } else {
          index = 0;
        }
-     } else {
-       index = 0;
-     }
-   });
- }
+     });
+   }
+}
